d7357a161d579494d56ac38e014b69bda409eee2
Kirill Pantsiuk <64860677+kirillpantsiuk@users.noreply.github.com>
2026-06-10 11:59:59 +0300

    Lesson_3

---
+++ Basic_JS/Lesson_3/js/my_script.js
@@ -0,0 +1,22 @@
+
+//var name = prompt("What is your name?");//модальне вікно з полем вводу 
+//document.write("Hello "+ name)
+//if else if else
+/*
+if (name=="Kyrylo") {
+     document.write("Hello, " + name);
+ } else if (name=="Dmytro") {
+        document.write("Hello, " + name);
+     }
+ else {
+    document.write("Hello, Guest!");
+}
+*/
+
+//функції виводу
+/*
+1.alert() - виводить повідомлення у вигляді модального вікна з кнопкою "OK"
+2.document.write() - виводить текст безпосередньо на сторінку
+3.console.log() - виводить повідомлення у консоль розробника (корисно для налагодження коду)
+*/
+console.log("Hello, World!");
